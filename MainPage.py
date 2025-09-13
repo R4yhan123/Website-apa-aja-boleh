2cb1560bde85f36020d79dd21d604839c6a254c3
R4yhan123 <rayhananjaya123@gmail.com>
2025-09-13 13:44:45 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -3,6 +3,8 @@ import streamlit as st
 st.set_page_config(
   page_title="ciri ciri orang kampung",
 )
-st.title("jam 5")
-st.subheader("aduhh kebelet eek")
-
+st.title("ciri ciri orang kampung sebagai berikut")
+st.subjudul("1.Dio")
+st.subjudul("2.Dio kampung")
+st.subjudul("3.Dio mesum,cabul,eci,pedo,hentai")
+st.title("sekian terimakasi")
