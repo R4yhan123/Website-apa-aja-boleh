4677e6b41bd24456497b6018e5af71bbf69a5bd3
R4yhan123 <rayhananjaya123@gmail.com>
2025-09-13 13:50:47 +0700

    Update MainPage.py

--- MainPage.py
+++ MainPage.py
@@ -4,8 +4,10 @@ st.set_page_config(
   page_title="ciri ciri orang kampung",
 )
 st.title("ciri ciri orang kampung sebagai berikut")
-st.subheader("1.Diohttps://github.com/R4yhan123/Website-apa-aja-boleh/edit/main/MainPage.py")
+st.subheader("1.orang yang bernama Dio")
 st.subheader("2.Dio kampung")
 st.subheader("3.Dio mesum,cabul,eci,pedo,hentai")
 st.title("sekian terimakasi")
 
+st.subheader("Rating seberapa mesum dio")
+st.number_input("Kasih Rating")
